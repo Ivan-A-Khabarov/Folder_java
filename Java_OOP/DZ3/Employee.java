9092ed5b1f67118c8831f2ab286895cb6310304e
Ivan-Khabarov <ivan@ihabarov.ru>
2024-03-05 21:45:59 +0300

    Добавлено решения по дз 3

---
+++ Java_OOP/DZ3/Employee.java
@@ -0,0 +1,2 @@
+package DZ3;public class Employees {
+}
---
+++ Java_OOP/DZ3/MyLinkedList.java
@@ -0,0 +1,94 @@
+package DZ3;
+
+import java.util.Iterator;
+
+class MyLinkedList<T> implements Iterable<T> {
+    private Node<T> head;
+
+    public void add(T data) {
+        if (head == null) {
+            head = new Node<>(data);
+        } else {
+            Node<T> current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = new Node<>(data);
+        }
+    }
+
+    public void remove(T data) {
+        if (head == null) {
+            return;
+        }
+        if (head.data.equals(data)) {
+            head = head.next;
+            return;
+        }
+        Node<T> current = head;
+        while (current.next != null) {
+            if (current.next.data.equals(data)) {
+                current.next = current.next.next;
+                return;
+            }
+            current = current.next;
+        }
+    }
+
+    public T get(int index) {
+        Node<T> current = head;
+        for (int i = 0; i < index; i++) {
+            if (current == null) {
+                return null;
+            }
+            current = current.next;
+        }
+        if (current == null) {
+            return null;
+        }
+        return current.data;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            private Node<T> current = head;
+
+            @Override
+            public boolean hasNext() {
+                return current != null;
+            }
+
+            @Override
+            public T next() {
+                T data = current.data;
+                current = current.next;
+                return data;
+            }
+        };
+    }
+
+    private static class Node<T> {
+        T data;
+        Node<T> next;
+
+        Node(T data) {
+            this.data = data;
+        }
+    }
+}
+
+class MyLinkedListTest {
+    public static void main(String[] args) {
+        MyLinkedList<Integer> list = new MyLinkedList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+
+
+        for (Integer i : list) {
+            System.out.println(i);
+        }
+    }
+}
